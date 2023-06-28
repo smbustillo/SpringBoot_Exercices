1d5c0d24072465ee7a86ae65199780e6ace50475
Susana M. Bustillo <smbustill@gmail.com>
2023-06-28 20:42:30 +0200

    hacerlo dinámico

--- src/main/java/com/example/JavaCurso/MainController.java
+++ src/main/java/com/example/JavaCurso/MainController.java
@@ -1,6 +1,7 @@
 package com.example.JavaCurso;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,16 +19,22 @@ public class MainController {
         return "P" + Math.round(num);
     }
 
-    @GetMapping("translate-to-morse")
-    public String transToMorse(){
-        String texto = "ABAABBA";
+    @GetMapping("translate-to-morse/{texto}")
+    public String transToMorse(@PathVariable String texto){
         String salida = "";
 
-        String [] letras = {"A","B","C"};
-        String [] morse = {".-","-...",".-."};
+        String[] letras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","_"};
+        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..","   "};
+
 
         for(int i = 0; i < texto.length(); i++){
-            char letra = texto.charAt(i);
+            String letra = "" + texto.charAt(i);
+
+            for(int j=0; j< morse.length; j++) {
+                if(letra.equalsIgnoreCase(letras[j])){
+                    salida += morse[j] + " ";
+                }
+            }
         }
 
         return salida;
